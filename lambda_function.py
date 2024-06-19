9f7df54785033db0b87f7cf59b40e8cd6d94fe25
Camilo <camilo.gdonoso@gmail.com>
2024-06-19 11:38:10 -0400

    added main.tf

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,8 @@
+import json
+
 def lambda_handler(event, context):
+    # TODO implement
     return {
         'statusCode': 200,
-        'body': 'Hello from Lambda!'
+        'body': json.dumps('Hallo aus Lambda!')
     }
---
+++ main.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_iam_role" "lambda_execution_role" {
+  name = "lambda_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Effect = "Allow"
+        Sid    = ""
+      },
+    ]
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
+  ]
+}
+
+resource "aws_lambda_function" "lambda_function" {
+  function_name = "my-lambda-function"
+  role          = aws_iam_role.lambda_execution_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.8"
+
+  filename      = "${path.module}/lambda_function.zip"
+
+  source_code_hash = filebase64sha256("${path.module}/lambda_function.zip")
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
+}
